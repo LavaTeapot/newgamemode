ff2ac8fab7a62a32e0d7503d41e83a1dfc417318
LavaTeapot <lavateapot@gmail.com>
2014-04-06 18:36:59 +0100

    Update init.lua

--- newgamemode/gamemode/init.lua
+++ newgamemode/gamemode/init.lua
@@ -8,3 +8,10 @@ function GM:PlayerIntialSpawn( ply )
   ply:SetWalkSpeed( 225 )
   ply:SetRunSpeed( 350 )
 end
+
+function GM:PlayerSpawn(ply)
+	if ply:Team() == 1 Then
+	ply:Give('weapon_pistol')
+	elseif ply:Team() == 2 Then
+	ply:Give('weapon_AR2')
+end
