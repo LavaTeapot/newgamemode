88ec5b9fee7b836e9e715c704e7c0b347fa70021
LavaTeapot <lavateapot@gmail.com>
2014-04-06 18:20:29 +0100

    Update init.lua

--- newgamemode/gamemode/init.lua
+++ newgamemode/gamemode/init.lua
@@ -7,9 +7,5 @@ function GM:PlayerIntialSpawn( ply )
   ply:SetGravity( 1 )
   ply:SetWalkSpeed( 225 )
   ply:SetRunSpeed( 350 )
-  ply:Give("weapon_AR2")
-end
-
-function GM:PlayerSpawn( ply )
   ply:Give("weapon_crowbar")
 end
