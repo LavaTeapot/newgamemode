2ea31fb4f36fda2687583383c8897c4bed228e7f
LavaTeapot <lavateapot@gmail.com>
2014-04-06 18:20:08 +0100

    Update init.lua

--- newgamemode/gamemode/init.lua
+++ newgamemode/gamemode/init.lua
@@ -2,3 +2,14 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 
 include( "shared.lua" )
+
+function GM:PlayerIntialSpawn( ply )
+  ply:SetGravity( 1 )
+  ply:SetWalkSpeed( 225 )
+  ply:SetRunSpeed( 350 )
+  ply:Give("weapon_AR2")
+end
+
+function GM:PlayerSpawn( ply )
+  ply:Give("weapon_crowbar")
+end
