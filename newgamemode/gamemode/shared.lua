4b96d164c2da1b74fc43234a6a2c4613293fc79c
LavaTeapot <lavateapot@gmail.com>
2014-04-07 10:38:22 +0100

    Update shared.lua

--- newgamemode/gamemode/shared.lua
+++ newgamemode/gamemode/shared.lua
@@ -7,5 +7,5 @@ function GM:Initialize()
 	self.BaseClass.Initialize(self)
 end 
 
-team.SetUp(1, 'TEAM1', Color(100,0,255))
+team.SetUp(1, 'TEAM1', Color(100, 0, 255))
 team.SetUp(2, 'TEAM2', Color(0, 100, 200))
